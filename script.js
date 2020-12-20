0a19d9299f1ff77dfd0188336caa3b10cb2e6697
Aziz Booker <bookeraziz@gmail.com>
2020-12-20 16:11:41 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,62 @@
+
+function add (num1,num2) {
+	return num1+num2;
+}
+
+function subtract (num1,num2) {
+	return num1-num2;
+	
+}
+
+function sum (arg) {
+	var sum=0
+	for(var i=0;i<arg.length;i++){
+	 sum+=arg[i]
+	}
+	return sum;
+}
+
+function multiply (arg) {
+	var ans=1;
+	for(var i=0;i<arg.length;i++){
+	 ans*=arg[i]
+	}
+	return ans;
+	
+	
+}
+
+function power(num1,num2) {
+	return num1**num2;
+}
+
+function factorial(num) {
+	var factorial=1
+
+	for(var i=num;i>0;i--){
+		factorial*=i;
+	}
+return factorial
+	
+}
+const currentNum=document.querySelector('#currentNum')
+
+function onButtonClick(btn){
+    
+    currentNum.textContent+=(btn.value)
+    
+}
+
+
+
+
+/*
+module.exports = {
+	add,
+	subtract,
+	sum,
+	multiply,
+    power,
+	factorial
+}
+*/
